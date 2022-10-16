b82c6bb8c7c29ae95ca388377710fc680c95ed48
Manish Rane <manishrane182@gmail.com>
2022-10-16 11:50:42 +0530

    Weather App

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ document.getElementById('date').innerHTML = days[d.getDay()] + ", " + d.getDate(
 
 
 function background(id){
-    if(id>=1000 && 1030>=id){
+    if(id>=800 && 804>=id){
         let cloud = document.getElementById("weather-icon");
         cloud.src="cloudy.png"
         document.body.style.backgroundImage = "url('cloud.gif')";
@@ -23,7 +23,7 @@ function background(id){
         document.getElementById('note').style.background = "rgba(255,255,255, 0.5)"
         document.getElementById('note').innerHTML="Its great time to explore outdoors !!"
 
-    } else if(id>=1063 && 1072>=id || id>=1150 && 1201>=id || id>=1240 && 1246>=id){
+    } else if(id>=300 && 532>=id){
         let rain = document.getElementById("weather-icon");
         rain.src="rainy.png"
         document.body.style.backgroundImage = "url('rain.gif')";
@@ -38,7 +38,7 @@ function background(id){
         document.getElementById('note').style.background = "transparent"
         document.getElementById('note').innerHTML="Don't forget your umbrella !!"
 
-    } else if(id>=1204 && 1237>=id || id>=1249 && 1264>=id || id>=1114 && 1135>=id){
+    } else if(id>=600 && 622>=id){
         let snow = document.getElementById("weather-icon");
         snow.src="snow.png"
         document.body.style.backgroundImage = "url('snow.gif')";
@@ -53,7 +53,7 @@ function background(id){
         document.getElementById('note').style.background = "rgba(255,255,255, 0.5)"
         document.getElementById('note').innerHTML="Its pretty chilly out there !!"
 
-    } else if (id==1087 || id>=1273 && 1282>=id){
+    } else if (id>=200 && 232>=id){
         let thunder = document.getElementById("weather-icon");
         thunder.src="thunder.png"
         document.body.style.backgroundImage = "url('thunder.gif')";
@@ -67,37 +67,54 @@ function background(id){
         document.getElementById('display').style.background = "transparent"
         document.getElementById('note').style.background = "transparent"
         document.getElementById('note').innerHTML="Thunderstorm is brewing better stay inside !!"
+    } else if(id>=701 && 781>=id){
+        let haze = document.getElementById("weather-icon");
+        haze.src="snow.png"
+        document.body.style.backgroundImage = "url('Haze.gif')";
+        document.getElementById('city').style.color = "#000000";
+        document.getElementById('date').style.color = "#000000";
+        document.getElementById('weather').style.color = "#000000";
+        document.getElementById('description').style.color = "#000000";
+        document.getElementById('temp').style.color = "#000000";
+        document.getElementById('note').style.color = "#000000";
+        document.getElementById('info-button').style.color = "#000000";
+        document.getElementById('display').style.background = "rgba(255,255,255, 0.5)"
+        document.getElementById('note').style.background = "rgba(255,255,255, 0.5)"
+        document.getElementById('note').innerHTML="Its pretty foggy out there, Drive safe !!"
     }
 }
 
 function weatherInfo(key){
     givedate();
-    axios.get('http://api.weatherapi.com/v1/current.json?key=bf8e2aaac0b94d11aa794209221510&q='+key+'&aqi=no')
+    axios.get('https://api.openweathermap.org/data/2.5/weather?q='+key+'&appid=ceddbeb05632f89ad9c335ae2abe113b&units=metric')
     .then (response =>{
-      let cityName = response.data.location.name+", "+response.data.location.country;
-      document.getElementById('city').innerText = cityName;
-
-      document.getElementById('description').innerText = response.data.current.condition.text;
-      document.getElementById('temp').innerHTML = response.data.current.temp_c+"<sup>o</sup> C";
-
-      let id = response.data.current.condition.code;
-      background(id);
+        let cityName = response.data.name+", "+response.data.sys.country;
+        document.getElementById('city').innerText = cityName;
+        document.getElementById('temp').innerHTML = response.data.main.temp+"<sup>o</sup> C";
+        response.data.weather.map((val)=>
+          {
+            // console.log(val.description);
+            document.getElementById('description').innerText = val.description;
+            // console.log(val.id);
+            let id = val.id;
+            background(id);
+          })
     })
   }
 
   function func(){
-    let state=true;
+    // let state=true;
     let key = document.getElementById('input-box').value
-    axios.get('http://api.weatherapi.com/v1/search.json?key=bf8e2aaac0b94d11aa794209221510&q='+key)
+    axios.get('http://api.openweathermap.org/geo/1.0/direct?q='+key+'&limit=1&appid=ceddbeb05632f89ad9c335ae2abe113b')
     .then (response =>{
         (response.data.map((val)=>{
-            if (state==true){
+            // if (state==true){
                 weatherInfo(val.name+","+val.country);
-                state=false;
-            }
+                // state=false;
+            // }
         }))
     })
-    .catch( error => console.error(error) );
+    .catch( error => document.getElementById('city').innerText = 'City Not Found' );
     }
 
 function guide(){
